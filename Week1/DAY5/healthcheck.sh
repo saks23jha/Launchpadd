19732b78dc78322a98ff15c1c93a5f3f25bb7f26
saks23jha <sakshij.hestabit@gmail.com>
2025-12-16 13:26:45 +0530

    husky passed

---
+++ Week1/DAY5/healthcheck.sh
@@ -0,0 +1,15 @@
+# #!/bin/bash
+
+# SERVER="8.8.8.8"   # Replace with your server IP or domain
+# LOG_FILE="logs/health.log"
+
+# while true
+# do
+#   if ping -c 1 $SERVER > /dev/null 2>&1
+#   then
+#     echo "$(date) - Server is UP"
+#   else
+#     echo "$(date) - Server DOWN" >> $LOG_FILE
+#   fi
+#   sleep 10
+# done
